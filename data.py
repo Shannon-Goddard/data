336058370f0147eda1a9ce6bb2519c4b433cbefb
Shannon Goddard <Uthingkithunk@aol.com>
2020-04-10 15:09:41 -0700

    Add files via upload

--- data.py
+++ data.py
@@ -4,5 +4,5 @@ import numpy as np
 import io
 url='https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv'
 df=pd.read_csv(url)
-df.to_json (r'C:\Users\User\Desktop\data\data.json')
+df.to_json('data.json', orient='records')
 
