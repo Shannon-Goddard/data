2e811544bb9ca35aa93425aa5ac19644894a6a9a
Shannon Goddard <Uthingkithunk@aol.com>
2020-04-10 17:01:03 -0700

    Add files via upload

--- data.py
+++ data.py
@@ -3,6 +3,15 @@ import pandas as pd
 import numpy as np
 import io
 url='https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv'
-df=pd.read_csv(url)
+df=pd.read_csv(url, dtype={
+            "date" : str,
+            "country" : str,
+            "state" : str,
+            "fibs" : str,
+            "cases" : str,
+            "death" : str
+        })
+df=df.drop(columns=['fips'])
+df["id"]=df["county"]
 df.to_json('data.json', orient='records')
 
