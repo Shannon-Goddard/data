fd96d1085db8f39af9468fe9ef97c754ff440fad
Shannon Goddard <Uthingkithunk@aol.com>
2020-04-28 07:47:19 -0700

    adding jupyter notebook and new python code

--- data.py
+++ data.py
@@ -3,15 +3,19 @@ import pandas as pd
 import numpy as np
 import io
 url='https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv'
-df=pd.read_csv(url, dtype={
-            "date" : str,
-            "country" : str,
-            "state" : str,
-            "fibs" : str,
-            "cases" : str,
-            "death" : str
-        })
-df=df.drop(columns=['fips'])
-df["id"]=df["county"]
+df=pd.read_csv(url)
+us_state_abbrev = {
+'Alabama': 'AL', 'Alaska': 'AK', 'Arizona': 'AZ', 'Arkansas': 'AR', 'California': 'CA', 'Colorado': 'CO',
+'Connecticut': 'CT', 'Delaware': 'DE', 'Florida': 'FL', 'Georgia': 'GA', 'Hawaii': 'HI', 'Idaho': 'ID',
+'Illinois': 'IL', 'Indiana': 'IN', 'Iowa': 'IA', 'Kansas': 'KS', 'Kentucky': 'KY', 'Louisiana': 'LA',
+'Maine': 'ME', 'Maryland': 'MD', 'Massachusetts': 'MA', 'Michigan': 'MI', 'Minnesota': 'MN', 'Mississippi': 'MS',
+'Missouri': 'MO', 'Montana': 'MT', 'Nebraska': 'NE', 'Nevada': 'NV', 'New Hampshire': 'NH', 'New Jersey': 'NJ',
+'New Mexico': 'NM', 'New York': 'NY', 'North Carolina': 'NC', 'North Dakota': 'ND', 'Ohio': 'OH', 'Oklahoma': 'OK',
+'Oregon': 'OR', 'Pennsylvania': 'PA', 'Rhode Island': 'RI', 'South Carolina': 'SC', 'South Dakota': 'SD',
+'Tennessee': 'TN', 'Texas': 'TX', 'Utah': 'UT', 'Vermont': 'VT', 'Virginia': 'VA', 'Washington': 'WA',
+'West Virginia': 'WV', 'Wisconsin': 'WI', 'Wyoming': 'WY'}
+df['state'] = df['state'].map(us_state_abbrev).fillna(df['state'])
+df = df.rename(columns={'county': 'area_name'})
+df = df.fillna(0)
+df.fips = df.fips.astype(int)
 df.to_json('data.json', orient='records')
-
